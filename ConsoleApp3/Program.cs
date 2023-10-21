fdd8c464aaac8d4b31a41cda10f219c0a0ce9e16
Patryk Świetlik <patrykswietlik2003@gmail.com>
2023-10-21 22:38:16 +0200

    Nowość

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace lekcja3
+    {
+        internal class Program
+{
+    private static void Main(string[] args)
+    {
+            int a = 10;
+
+            Console.WriteLine(a);
+            if (a > 0 && a > 7)
+            {
+                Console.WriteLine("A jest większa od 0");
+                Console.WriteLine("A jest większa od 7");
+            }
+            else if (a == 0) 
+            {
+                Console.WriteLine("A jest równe 0");
+            }
+            else
+            {
+                Console.WriteLine("A jest mniejsze od 0");
+            }
+            Console.ReadKey();
+
+    }
+  }
+}

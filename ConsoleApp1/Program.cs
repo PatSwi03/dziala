01ba27a6b1ebcd93f572363bfbd2717cc880b1e8
Patryk Świetlik <patrykswietlik2003@gmail.com>
2023-10-21 20:55:38 +0200

    Aktualizacja

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,20 @@
     {
         static void Main(string[] args) 
         {
-            int zmienna = int.MaxValue;
-            Console.WriteLine(zmienna);
-            var zmienna2 = "Hello";
+            float zmienna4 = 1.121F;
+            double zmienna5 = 1.21;
+            decimal zmienna6 = 9.12M;
+
+
+            string zmienna3 = "Hej";
+            char zmienna14 = 's';
+            bool zmienna2 = 5 > 2 && 5 < 3;
+            Console.WriteLine(zmienna2);
+            Console.WriteLine("Siema nema neczko");
+            Console.WriteLine(zmienna4);
+            Console.WriteLine(zmienna5);
+            Console.WriteLine(zmienna6);
+            Console.WriteLine(zmienna14);
             Console.ReadKey();
         }
     }

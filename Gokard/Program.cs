8e0d3b5a87d6c19c3194e888e60da0e464f576b9
Patryk Świetlik <patrykswietlik2003@gmail.com>
2023-11-05 09:55:57 +0100

    akt

--- Gokard/Program.cs
+++ Gokard/Program.cs
@@ -22,14 +22,19 @@ namespace Gokardy
         static void Main(string[] args)
         {
             Zawodnik anonim = new Zawodnik(); 
-            anonim.id = int.Parse(Console.ReadLine());
-            anonim.imie = "Adam";
-            anonim.nazwisko = "Kowalski";
-            anonim.wiek = 26;
-            anonim.plec = true;
-            anonim.poziom = false;
-            anonim.ranking = 3;
-            
+            anonim.id = int.Parse(pobierzDane("Podaj id"));
+            anonim.imie = pobierzDane("Podaj imię");
+            anonim.nazwisko = pobierzDane("Podaj nazwisko");
+            anonim.wiek = byte.Parse(pobierzDane("Podaj wiek"));
+            anonim.plec = bool.Parse(pobierzDane("Podaj płeć"));
+            anonim.poziom = bool.Parse(pobierzDane("Podaj poziom"));
+            anonim.ranking = int.Parse(pobierzDane("Podaj ranking"));
+
+        }
+        static string pobierzDane(string doWyswietlenia)
+        {
+            Console.WriteLine(doWyswietlenia);
+            return Console.ReadLine();
         }
     }
 }

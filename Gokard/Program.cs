1343e817b581ffa9e482878651b08443ec833b37
Patryk Świetlik <patrykswietlik2003@gmail.com>
2023-11-05 09:35:13 +0100

    aktualka

--- Gokard/Program.cs
+++ Gokard/Program.cs
@@ -8,17 +8,28 @@ namespace Gokardy
 {
     class Program
     {
-        int id;
-        string imie;
-        string nazwiko;
-        bool plec;
-        byte wiek;
-        bool poziom;
-        int ranking;
 
+        public struct Zawodnik
+        {
+           public int id;
+           public string imie;
+           public string nazwisko;
+           public bool plec;
+           public byte wiek;
+           public bool poziom;
+           public int ranking;
+        }
         static void Main(string[] args)
         {
-
+            Zawodnik anonim = new Zawodnik(); 
+            anonim.id = int.Parse(Console.ReadLine());
+            anonim.imie = "Adam";
+            anonim.nazwisko = "Kowalski";
+            anonim.wiek = 26;
+            anonim.plec = true;
+            anonim.poziom = false;
+            anonim.ranking = 3;
+            
         }
     }
 }

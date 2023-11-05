6f4a4c8df401043e3387e81f2fa3df7e57c1c832
Patryk Świetlik <patrykswietlik2003@gmail.com>
2023-11-05 08:57:30 +0100

    aktualizacja

--- Gokard/Program.cs
+++ Gokard/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gokardy
+{
+    class Program
+    {
+        int id;
+        string imie;
+        string nazwiko;
+        bool plec;
+        byte wiek;
+        bool poziom;
+        int ranking;
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}

90a3a19e1d4c48e77d39c1b477b6aadbb2fe7bb7
Patryk Świetlik <patrykswietlik2003@gmail.com>
2023-11-25 11:08:12 +0100

    321

--- 25.11.23/25.11.23/Program.cs
+++ 25.11.23/25.11.23/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace nowy
+{
+    internal class Program
+    {
+        static int zmienna1 = 1;
+        static byte zmienna2 = 2;
+        static double zmienna3 = 124124124.14;
+        static string zmiennax = "";
+
+        static void Main(string[] args)
+        {
+            //zmienna1 = zmienna2;
+            procedure2();
+        }
+
+        static void procedure2()
+        {
+            // zmienna2 = (byte)zmienna3;
+            // zmiennax=zmienna3.ToString();
+            zmienna2 = Convert.ToByte(zmienna3);
+        }
+    }
+}

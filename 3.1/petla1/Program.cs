1ef678deed85ce561690d93c8eaa077357f15df7
Patryk Świetlik <patrykswietlik2003@gmail.com>
2023-12-09 18:29:29 +0100

    zrobione

--- 3.1/petla1/Program.cs
+++ 3.1/petla1/Program.cs
@@ -1,7 +1,14 @@
-﻿internal class Program
+﻿using System;
+
+class Program
 {
-    private static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+       for (int x = 0; x <= 10; x++)
+       {
+           double y = Math.Pow(3, 2) * Math.Sqrt(x);
+           Console.WriteLine("Dla x wartość wynosi = " + x +", dla y wartość wynosi ="+y);
+       }
+       Console.ReadKey();
     }
 }

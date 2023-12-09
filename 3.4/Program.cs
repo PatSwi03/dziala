d6a3738421ef7a7bb9eb1c4f0ede84e6db2e9130
Patryk Świetlik <patrykswietlik2003@gmail.com>
2023-12-09 19:28:08 +0100

    cat

---
+++ 3.4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] pets = {"cat", "dog", "parrot", "guinea pig"};
+
+        foreach (string pet in pets)
+        {
+            if (pet == "dog")
+            {
+                Console.WriteLine("End");
+                break;
+            }
+            Console.WriteLine(pet);
+        }
+        Console.ReadKey();
+    }
+}

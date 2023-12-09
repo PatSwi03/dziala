19da1ebf7d6489ac5e0ffd6d398563db4748a4a1
Patryk Świetlik <patrykswietlik2003@gmail.com>
2023-12-09 19:16:28 +0100

    zroobionne

---
+++ 3.3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = 1;
+        do
+        {
+            int square = x * x;
+            Console.WriteLine("Kwadrat liczby " + x + square);
+
+            x++;
+        } while (x <= 20);
+        Console.ReadKey();
+    }
+}
